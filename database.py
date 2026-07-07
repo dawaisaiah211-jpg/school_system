75c003130be0b2dc00a3b37f58675a629728395b
Your Isadaw <dawaisaiah211@gmail.com>
2026-07-07 16:26:31 +0300

    Use environment variable for database password

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
+import os
 import mysql.connector
 
 def get_db():
     conn = mysql.connector.connect(
-        host="127.0.0.1",
-        user="shule_admin",
-        password="Shule@2026",
-        database="school_db"
+        host="mysql-325837d0-dawaisaiah211-4d7a.h.aivencloud.com",
+        port=11209,
+        user="avnadmin",
+        password=os.environ.get("DB_PASSWORD"),
+        database="defaultdb"
     )
     return conn
