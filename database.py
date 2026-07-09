e1b7677fbc32355f4c1b011967e03c80d978dc23
Your Isadaw <dawaisaiah211@gmail.com>
2026-07-09 16:19:26 +0300

    git Debug DB Host

--- database.py
+++ database.py
@@ -1,13 +1,15 @@
+
+
 import os
 import mysql.connector
 
 def get_db():
-    connection = mysql.connector.connect(
+    print("DB_HOST =", os.environ.get("DB_HOST"))
+
+    return mysql.connector.connect(
         host=os.environ.get("DB_HOST"),
         port=int(os.environ.get("DB_PORT")),
         database=os.environ.get("DB_NAME"),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
-        ssl_disabled=False
     )
-    return connection
